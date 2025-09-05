61834b7f2dffb8938ab5da109ec56ab032882633
MatNgl <mattheo.naegellen@gmail.com>
2025-09-05 12:08:55 +0200

    fix: auto scroll to top

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 
+
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+  ],
 });
