3156ea081106abc466424c3bde97631eac083b0b
MatNgl <mattheo.naegellen@gmail.com>
2025-09-24 21:31:05 +0200

    fix: move somes files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { errorInterceptor } from './shared/interceptors/error.interceptor';
-import { GlobalErrorHandler } from './shared/handlers/global-error.handler';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { GlobalErrorHandler } from './core/handlers/global-error.handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
